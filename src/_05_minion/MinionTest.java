30715944aba43ef367a2fcea948337b2a0c077d9
student <student@ip-172-31-55-193.us-west-2.compute.internal>
2021-06-05 12:49:32 -0700

    Level 1 Module 2 Almost done

--- src/_05_minion/MinionTest.java
+++ src/_05_minion/MinionTest.java
@@ -33,6 +33,35 @@ This is because there is no Minion class in the package.
     
  */
 
+class Minion{
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+	public Minion(String name, int eyes, String color, String master){
+		this.name = name;
+		this.eyes = eyes;
+		this.color = color;
+		this.master = master;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getEyes() {
+		return eyes;
+	}
+	public String getColor() {
+		return color;
+	}
+	public String getMaster() {
+		return master;
+	}
+	public void setMaster(String master) {
+		this.master = master;
+	}
+}
+
+
 public class MinionTest {
 
 	@Test
