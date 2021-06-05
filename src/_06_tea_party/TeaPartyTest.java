56c9f54827f08c10f41c074c7d339b75c08f7d8d
student <student@ip-172-31-55-193.us-west-2.compute.internal>
2021-06-05 12:49:53 -0700

    Level 1 Module 2

--- src/_06_tea_party/TeaPartyTest.java
+++ src/_06_tea_party/TeaPartyTest.java
@@ -1,5 +1,46 @@
 package _06_tea_party;
 
-public class TeaPartyTest {
+public class TeaParty {
+	String name;
+	boolean isWoman;
+	boolean isKnighted;
+	public TeaParty(String name, boolean isWoman, boolean isKnighted){
+		
+	}
+	
+    public String welcome(String name, boolean isWoman, boolean isKnighted) {
+        
+    }
+}
 
+public class TeaPartyTest {
+	 /**
+	 * Jane Austen is a woman, so say “Hello Ms. Austen”. 
+	 * George Orwell is a man, so say “Hello Mr. Orwell”. 
+	 * Isaac Newton was knighted, so say "Hello Sir Isaac Newton".
+	 **/
+
+	@Test
+	public void testIsWomanNotKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false));
+	}
+
+	@Test
+	public void testIsManNotKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false));
+	}
+
+	@Test
+	public void testIsManKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true));
+	}
+	
+	@Test
+	public void testIsWomanKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Lady Laura", teaParty.welcome("Laura", true, true));
+	}
 }
